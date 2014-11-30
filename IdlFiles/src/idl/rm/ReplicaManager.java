11a16aa1838db23d099eda573c90339ff5d74324
Gallindo, Gustavo <gugagreen@gmail.com>
2014-11-29 22:04:24 -0500

    idl project

---
+++ IdlFiles/src/idl/rm/ReplicaManager.java
@@ -0,0 +1,36 @@
+package idl.rm;
+
+public interface ReplicaManager {
+
+	/**
+	 * Process failure notification from the Front End. If the same replica produces incorrect results for three 
+	 * consecutive client requests, then the replica is replaced.
+	 * 
+	 * @param replicaName
+	 * @param rmId
+	 */
+	void handleFailure(String replicaName, String rmId);
+	
+	/**
+	 * Launch heartbeats to other replica managers to check if their replicas are alive.
+	 */
+	void launchHeartBeats();
+	
+	/**
+	 * Process heartbeat from other ReplicaManager to check if a specific replica is alive.
+	 * 
+	 * @param replicaName
+	 * @return true if replica is alive
+	 */
+	boolean processHeartBeat(String replicaName);
+	
+	/**
+	 * Process crash agreement request issued by another ReplicaManager.
+	 * 
+	 * @param crashedReplicaName
+	 * @param crashedRmId
+	 * @param notifierRmId
+	 * @return true if this ReplicaManager agrees the replica has crashed.
+	 */
+	boolean processCrashAgreement(String crashedReplicaName, String crashedRmId, String notifierRmId);
+}
